6b5d4783931a85b2e4fe2ec77c3c3d1443ac56ca
S-A-M-J <60389168+S-A-M-J@users.noreply.github.com>
2022-10-29 14:48:54 +0200

    Update wordclockConfigurator.js

--- wordclockConfigurator.js
+++ wordclockConfigurator.js
@@ -22,7 +22,7 @@ let tcpEnabled = false;
 //let binFileInput;
 
 function preload() {
-  wordclocklogoImg = loadImage('data/wordclock_conf_logo.PNG');
+  wordclocklogoImg = loadImage('data/wordclock_conf_logo.png');
 }
 
 function setup() {
