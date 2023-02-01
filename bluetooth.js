9e0ead3b96779a1952bc00d09cca8e171425fc42
S-A-M-J <60389168+S-A-M-J@users.noreply.github.com>
2023-02-01 15:31:09 +0100

    update

--- bluetooth.js
+++ bluetooth.js
@@ -36,7 +36,7 @@ function gotCharacteristics(error, characteristics) {
 
   blueTooth.startNotifications(blueToothRXCharacteristic, gotValue, 'string');
   isConnected = blueTooth.isConnected();
-
+  gotValue(" ");
   connectButton.hide();
   showAllParam();
   sendData("#param");
